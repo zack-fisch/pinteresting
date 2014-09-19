976e9ed118923fe698f8b26e34d582893d0a1dbf
Zack Fisch <zack@zfisch.com>
2014-09-19 09:30:40 -0400

    strapping up the boot laces for bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  
+  root 'pages#home'
+  match "/about" => "pages#about", :via => :get, as: :about
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
